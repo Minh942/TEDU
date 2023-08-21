e80648035648bfbf0336a5213e668b9353a9efb0
Minh Phan <minh.phan@ntq-solution.com.vn>
2023-08-21 16:39:03 +0700

    update database

--- TeduShop.Model/Models/OrderDetail.cs
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -5,12 +5,14 @@ using TeduShop.Model.Abstract;
 namespace TeduShop.Model.Models
 {
     [Table("OrderDetails")]
-    public class OrderDetail : Auditable
+    public class OrderDetail
     {
         [Key]
+        [Column(Order = 1)]
         public int OrderID { get; set; }
         [Key]
-        public int ProducID { get; set; }
+        [Column(Order = 2)]
+        public int ProductID { get; set; }
         public int Quantity { get; set; }
         [ForeignKey("OrderID")]
         public virtual Order Order { get; set; }
--- TeduShop.Model/Models/ProductTag.cs
+++ TeduShop.Model/Models/ProductTag.cs
@@ -7,9 +7,10 @@ namespace TeduShop.Model.Models
     public class ProductTag
     {
         [Key]
+        [Column(Order = 1)]
         public int ProductID { get; set; }
         [Key]
-        [Column(TypeName = "varchar")]
+        [Column(Order = 2 ,TypeName = "varchar")]
         [MaxLength(50)]
         public string TagID { get; set; }
         [ForeignKey("ProductID")]
