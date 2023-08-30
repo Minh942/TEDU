b0d6f58426ca28fb82aecd5c682aa8c5f73634fb
Minh Phan <minh.phan@ntq-solution.com.vn>
2023-08-30 08:54:22 +0700

    update

---
+++ TeduShop.Web/App_Start/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Autofac;
+using System;
+using System.Threading.Tasks;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using TeduShop.Data.Infrastructure;
+
+[assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
+
+namespace TeduShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+        }
+    }
+}
